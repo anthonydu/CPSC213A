fb597cd39193187a2d9bffff06bb71815212e5ea
Anthony Du <github@anthonydu.com>
2023-09-26 00:53:07 -0700

    Created test.s

--- A2/test.s
+++ A2/test.s
@@ -1,47 +1,58 @@
-.pos 0x1000
-li:   ld  $data, r1
-      ld  $0,    r2
-      ld  $1,    r3
-      ld  (r1,r2,4), r4     # r4 <= m[data + 0]
-      ld  (r1,r3,4), r5     # r5 <= m[data + 4]
-      halt
-si:   ld  $data, r1
-      ld  $0, r2
-      ld  $3, r3
-      ld  $1, r4
-      st  r3, (r1, r2, 4)   # m[data + 0] <= 3
-      st  r4, (r1, r4, 4)   # m[data + 4] <= 1
-      halt
-rmov: ld  $1, r1
-      mov r1, r2            # r2 <= 1
-      halt
-add:  ld  $1, r1
-      ld  $2, r2
-      add r1, r2            # r2 <= 1 + 2
-      halt
-and:  ld  $10, r1
-      ld  $6, r2
-      and r1, r2            # r2 <= 10 & 6 == 0b1010 & 0b0110 == 0b0010 == 2
-      halt
-inc:  ld  $1, r1
-      inc r1                # r1 <= 1 + 1
-      halt
-inca: ld  $1, r1
-      inca r1               # r1 <= 1 + 4
-      halt
-dec:  ld  $2, r1
-      dec r1                # r1 <= 2 - 1
-      halt
-deca: ld  $5, r1
-      deca r1               # r1 <= 5 - 4
-      halt
-not:  ld  $0xffff0000, r1
-      not r1                # r1 <= ! 0xffff0000 == 0x0000ffff
-      halt
-shl:  ld  $0x12345678, r1
-      shl $17, r1            # r1 <= 0x1234567 << 17 == 0xacf0000
-      halt
+.pos 0x100 # load a and b
+        ld $a, r0
+        ld 0(r0), r1
+        ld 4(r0), r2
+        halt
 
-.pos 0x2000
-data: .long 1
-      .long 2
+.pos 0x200 # load a and b
+        ld $a, r0
+        ld $0, r1
+        ld $1, r2
+        ld (r0, r1, 4), r1
+        ld (r0, r2, 4), r2
+        halt
+
+.pos 0x300 # swap a and b
+        ld $a, r0
+        ld (r0), r1
+        ld $b, r2
+        ld (r2), r2
+        st r2, (r0)
+        st r1, 4(r0)
+        halt
+
+.pos 0x400 # swap a and b
+        ld $a, r0
+        ld (r0), r1
+        ld $b, r2
+        ld (r2), r2
+        ld $0, r3
+        ld $1, r4
+        st r2, (r0, r3, 4)
+        st r1, (r0, r4, 4)
+        halt
+
+.pos 0x600 # all ALU's
+        ld $0x0f0f0f0f, r0
+        mov r0, r1
+        inc r0
+        inca r1
+        add r0, r1
+        and r1, r0
+        dec r0
+        deca r1
+        not r1
+        not r1
+        halt
+
+.pos 0x700 # shl, shr
+        ld $0x0f0f0f0f, r0
+        shl $3, r0
+        shr $5, r0
+        halt
+
+
+
+.pos 0x1000
+a:      .long 0x0f0f0f0f
+b:      .long 0xf0f0f0f0
