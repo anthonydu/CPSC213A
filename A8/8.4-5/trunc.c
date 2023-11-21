31be1a6fb75b7fdb7264a2a519d5de70d1714d48
Anthony Du <github@anthonydu.com>
2023-11-21 13:35:50 -0800

    Update trunc.c

--- A8/8.4-5/trunc.c
+++ A8/8.4-5/trunc.c
@@ -9,13 +9,12 @@
 int is_int(char* str) {
   char* endptr;
   strtol(str, &endptr, 10);
-  int result = str[0] != 0 && endptr[0] == 0;
-  endptr = NULL;
-  return result;
+  return str[0] != 0 && endptr[0] == 0;
 }
 
 void to_int(int* out, char* str) {
-  *out = strtol(str, NULL, 10);
+  char* endptr;
+  *out = strtol(str, &endptr, 10);
 }
 
 int is_str(char* str) {
@@ -23,9 +22,10 @@ int is_str(char* str) {
 }
 
 void trunc(char** out, int len, char* str) {
-  char* result = malloc(len);
+  char* result = malloc(len + 1);
+  // strncpy does not add null at the end of the resulting string
   *out = strncpy(result, str, len);
-  result = NULL;
+  result[len] = 0;
 }
 
 void print_vertical(char* str) {
@@ -43,33 +43,26 @@ void max(int* max_p, int max, int num2) {
 int main(int argc, char** argv) {
   struct list* args = list_create();
   list_append_array(args, argv + 1, argc - 1);
-
   struct list* int_strs = list_create();
   list_filter(is_int, int_strs, args);
-  struct list* strs = list_create();
-  list_filter(is_str, strs, args);
-  list_destroy(args);
-  args = NULL;
-
   struct list* ints = list_create();
   list_map1(to_int, ints, int_strs);
-  list_destroy(int_strs);
-  int_strs = NULL;
-
+  struct list* strs = list_create();
+  list_filter(is_str, strs, args);
   struct list* results = list_create();
   list_map2(trunc, results, ints, strs);
-  list_destroy(strs);
-  strs = NULL;
-
   list_foreach(print_vertical, results);
   list_foreach(print_horizontal, results);
+  int* max_num = malloc(sizeof(int));
+  // to prevent usage of uninitialized values
+  *max_num = 0;
+  list_foldl(max, max_num, ints);
+  printf("\n%d\n", *max_num);
   list_foreach(free, results);
-  list_destroy(results);
-  results = NULL;
-
-  int max_num = NULL;
-  list_foldl(max, &max_num, ints);
-  printf("\n%d\n", max_num);
+  list_destroy(args);
+  list_destroy(int_strs);
   list_destroy(ints);
-  ints = NULL;
+  list_destroy(strs);
+  list_destroy(results);
+  free(max_num);
 }
