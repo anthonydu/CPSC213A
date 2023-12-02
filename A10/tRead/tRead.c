de206c99fe415b4ba9579411af816e6c56fde24a
Anthony Du <github@anthonydu.com>
2023-12-02 11:15:43 -0800

    Added A10/tRead

---
+++ A10/tRead/tRead.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/errno.h>
+#include <assert.h>
+#include "queue.h"
+#include "disk.h"
+#include "uthread.h"
+
+queue_t      pending_read_queue;
+unsigned int sum = 0;
+
+void interrupt_service_routine () {
+  // TODO
+}
+
+void* read_block (void* blocknov) {
+  // TODO schedule read and the update (correctly)
+
+  return NULL;
+}
+
+int main (int argc, char** argv) {
+
+  // Command Line Arguments
+  static char* usage = "usage: tRead num_blocks";
+  int num_blocks;
+  char *endptr;
+  if (argc == 2)
+    num_blocks = strtol (argv [1], &endptr, 10);
+  if (argc != 2 || *endptr != 0) {
+    printf ("argument error - %s \n", usage);
+    return EXIT_FAILURE;
+  }
+
+  // Initialize
+  uthread_init (1);
+  disk_start (interrupt_service_routine);
+  pending_read_queue = queue_create();
+
+  // Sum Blocks
+  // TODO
+}
+
